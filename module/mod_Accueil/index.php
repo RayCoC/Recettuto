5cc345fe312c45627afbdd80e5481c71b4e48158
Rayan HAMOUCHE <rhamouche@a205-20.iut.univ-paris8.fr>
2021-12-03 10:11:18 +0100

    Rayan

--- module/mod_Accueil/index.php
+++ module/mod_Accueil/index.php
@@ -8,5 +8,30 @@
 
     <body>
         <input type ="text" id="searchBox">
+        <div id ="recette1">
+            <img src = "./img/gateau.png">
+            <img src = "./img/italie.png">
+        </div>
+        <div class ="CatégorieRecette">
+            <h1 class = "TitreCatégorie"> Dernières recettes </h1>
+            <img src="./img/Crepe.png">
+            <img src ="./img/italiaDelRepas">
+            <img src = "./img/Lasagne.png">
+        </div>
+        <div class= "CatégorieRecette">
+            <h1 class="TitreCatégorie"> Recettes saisonnières</h1>
+            <img src = "./img/Nouille.png">
+            <img src = "./img/cake.png">
+            <img src="./img/salade.png">
+        </div>
+        <div id = "ThemeRecette">
+            <h1 class="TitreCatégorie">Thémes de Recettes </h1>
+            <ul>
+                <li>Gateaux</li>
+                <li>Patate</li>
+                <li>Italie</li>
+                <li>Genre</li>
+            </ul>
+        </div>
     </body>
 </html>
